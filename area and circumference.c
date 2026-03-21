f5dd37f5907a558651519a039bb7fa5fa11d2f6c
25116cn380-coder <25116cn380@glbitm.ac.in>
2026-03-21 11:14:22 +0530

    Add files via upload

---
+++ area and circumference.c
@@ -0,0 +1,14 @@
+include <stdio.h>
+
+int main() {
+    float r,a,c;
+    printf("enter the value o fradius\n");
+    scanf("%f", &r);
+    c=(2*3.14*r);
+    printf("circumference = %f\n",c);
+    a=(3.14*r*r);
+    printf("area=%f/n",a);
+    
+
+    return 0;
+}
