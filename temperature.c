6ddee7f5520d91e9de5ebd43ff1cd93567f6632e
25116cn380-coder <25116cn380@glbitm.ac.in>
2026-03-21 11:21:55 +0530

    Add files via upload

---
+++ temperature.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    float c,f;
+    printf("ener the temperature in centigrade");
+    scanf("%f", &c);
+    f=(9/5)*c+32;
+    printf("temperaturein faranheit= %f",f);
+
+    return 0;
+}
