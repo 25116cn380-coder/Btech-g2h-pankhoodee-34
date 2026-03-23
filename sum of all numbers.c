2894e2f73fa350287bba2b7761d307659d627b1d
25116cn380-coder <25116cn380@glbitm.ac.in>
2026-03-23 20:01:14 +0530

    Add files via upload

---
+++ sum of all numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int n,i;
+    int sum = 0; 
+    printf("enter a number n");
+    scanf("%d", &n);
+    for(i=1; i<=n ; i++){
+        sum=sum+i;
+    }
+    printf("sum=%d", sum);
+    
+
+    return 0;
+}
